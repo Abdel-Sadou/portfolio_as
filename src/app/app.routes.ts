3d09992b456403c4e9215c4d6cd7ae55d25cd0fc
Abdel-Sadou <abdelsadou6@gmail.com>
2024-01-25 00:14:37 +0100

    footer ok (mais il reste l'animé) ; design projets ok(il reste ajouter mes propres projets); skills ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
-  {path:"home",component:AppComponent},
+  {path:"home",title:"Abdel • WebSite",component:AppComponent},
   {path:"", redirectTo:"/home", pathMatch:"full"}
 
 ];
