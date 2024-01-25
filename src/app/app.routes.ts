002e838facd26516aa90fa63bf8b912f8d27067a
Abdel-Sadou <abdelsadou6@gmail.com>
2024-01-25 02:20:07 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
-
-export const routes: Routes = [
-  {path:"home",title:"Abdel • WebSite",component:AppComponent},
-  {path:"", redirectTo:"/home", pathMatch:"full"}
-
-];
